f886c0f806b2172cdcc39dc300a9108162b88ed7
Marina Zalozna <marina.zalozna@gmail.com>
2023-02-11 15:16:08 +0100

    110223 HW

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,24 +4,23 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 
 export default function ContactList() {
-  const contacts = useSelector(state =>
-    state.contacts.items.filter(contact =>
-      contact.name.toLowerCase().includes(state.contacts.filter)
-    )
-  );
-
   const dispatch = useDispatch();
+  const filter = useSelector(state => state.filter.status);
+  const contacts = useSelector(state => state.contacts.items);
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
+  const getContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter)
+  );
   return (
     <ul className={css.contacts}>
-      {contacts.map(({ id, name, number }) => (
+      {getContacts.map(({ id, name, phone }) => (
         <li className={css.contact__list} key={id}>
           <span className={css.contact__item}>{name}: </span>
-          <span className="contact__item">{number} </span>
+          <span className="contact__item">{phone} </span>
           <button
             className={css.contacts__button}
             type="button"
